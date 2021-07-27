88ffb3c2b87b7e71054e42928a0a1a4cf81ae8f9
ilya <illia.kleshchev@gmail.com>
2021-07-27 15:29:33 +0300

    yes

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ import os
 from flask import Flask, render_template, request
 from cryptography.fernet import Fernet
 
+
 app = Flask(__name__)
-# add the key
+
+
 key = Fernet.generate_key()
 
+
 f = Fernet(key)
+
 
 token = f.encrypt(b"welcome to geeksforgeeks")
 d = f.decrypt(token)
 
-# http://127.0.0.1:5000/
 
 @app.route('/')
 def hello_word():
     os.getenv('PASSWORD')
     return "<h1>Hello world!</h1>"
+
 
 @app.route('/encrypt')
 def index():
